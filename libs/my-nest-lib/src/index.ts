48133566f8a0c7caa6b03a6266190185dcd93763
Michael ANTOUN <michael.antoun@fabernovel.com>
2022-07-29 11:52:04 +0200

    [ENV CONF] Export module, service, and type class validation for use in controller

---
+++ libs/my-nest-lib/src/index.ts
@@ -0,0 +1,4 @@
+// In index, we must export everything that we will use
+export * from './lib/my-nest-lib.module'
+export * from './lib/my-nest-lib.service'
+export * from '../config/environmentVariables'
